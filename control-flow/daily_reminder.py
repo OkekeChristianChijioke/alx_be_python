6ea5047c715627c30b2b0655660c3d35bc8272f8
Okeke Christian <okeke.christianchijioke@gmail.com>
2025-11-12 18:44:26 +0100

    task daily reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,7 +2,7 @@ task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 match priority:
-    case "high"|"medium"|"low":
+    case "high":
         if time_bound == "yes":
             print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
         else:
