09bdd602197e59dd29381b3d0a72f8648cbbf641
Okeke Christian <okeke.christianchijioke@gmail.com>
2025-11-19 20:03:17 +0100

    exploring datetime function in Python

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,7 +1,7 @@
-import datetime as dt
+from datetime import date, timedelta
 
 def display_current_datetime():
-    current_datetime = dt.datetime.now()
+    current_datetime = date.today()
     formatted_current_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
     print(f"Current date and time: {formatted_current_datetime}")
     return current_datetime  # return the actual datetime object
@@ -11,14 +11,11 @@ def calculate_future_date():
     current_datetime = display_current_datetime()
 
     no_of_days = int(input("Enter the number of days to add to the current date: "))
-    delta = dt.timedelta(days=no_of_days)
+    delta = timedelta(days=no_of_days)
 
-    future_date_only = current_datetime + delta
-
-    future_date = future_date_only.date()    # write only the date part
+    future_date = current_datetime + delta
 
     print(f"Future date: {future_date}")
-    return future_date_only   # or return future_datetime if you want date + time
 
 calculate_future_date()
 
