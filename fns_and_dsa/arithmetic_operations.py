24d5f1accf53d80c484751263057916f175cf8b4
Okeke Christian <okeke.christianchijioke@gmail.com>
2025-11-19 13:45:53 +0100

    arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,13 +9,12 @@ def perform_operation(num1, num2, operation):
         result = num1 * num2
         
     elif operation == "divide":
-        if num2 > 0:
-            result = num1 / num2
-        
-        else:
+        if num2 == 0:
             result = "Cannot divide by zero"
+            
+        else:
+            result = num1 / num2
     return result
-
 perform_operation(num1, num2, operation)
 
 
